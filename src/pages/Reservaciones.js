617cd7ef927b4c6838cf696a156d7f3764359cf5
MarioAs22 <mario.alca22@gmail.com>
2023-11-14 21:19:33 -0600

    Se creo la pagina de reservaciones

---
+++ src/pages/Reservaciones.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+function Reservaciones() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    date: '',
+    time: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes enviar los datos del formulario a un servidor o realizar otras acciones.
+    console.log('Datos de la reserva:', formData);
+  };
+
+  return (
+    <div>
+      <h2>Reserva en El Rincón de Poma</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nombre:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Correo Electrónico:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Teléfono:</label>
+          <input
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Fecha:</label>
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Hora:</label>
+          <input
+            type="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Reservar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Reservaciones;
